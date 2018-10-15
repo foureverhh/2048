fed302e87e29a6d781102168e8b912da2781dc68
foureverhh <foureverhh@gmail.com>
2018-10-15 19:30:14 +0200

    15. Added TileStyleHolder and TileStyle to define tile style

---
+++ 2048/Assets/Scripts/TilestyleHolder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TileStyle
+{
+    public int Number;
+    public Color32 TileColor;
+    public Color32 NumberColor;
+}
+
+public class TilestyleHolder : MonoBehaviour {
+
+    //SINGELTON
+    public static TilestyleHolder Instance;
+
+    public TileStyle[] tileStyles;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+}
