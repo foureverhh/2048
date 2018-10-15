c3dc2f1c27780c414ce9115d5fed159113aa0854
foureverhh <foureverhh@gmail.com>
2018-10-15 18:48:29 +0200

    14.Added GameManager and InputManager to realise Arrow control

---
+++ 2048/Assets/Scripts/InputManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MoveDirection
+{
+    Left,Right,Up,Down
+}
+
+public class InputManager : MonoBehaviour {
+
+    private GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = transform.GetComponent<GameManager>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //Right Move
+            gameManager.Move(MoveDirection.Right);
+        }else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //Left Move
+            gameManager.Move(MoveDirection.Left);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //Up Move
+            gameManager.Move(MoveDirection.Up);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            //Right Move
+            gameManager.Move(MoveDirection.Down);
+        }
+    }
+}
