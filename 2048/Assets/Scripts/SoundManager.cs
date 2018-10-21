f95c0355a7647a0c71286a1e21ef90f091bd99fe
foureverhh <foureverhh@gmail.com>
2018-10-21 21:07:14 +0200

    Added soundmanager as singleton

---
+++ 2048/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource effectMusic;
+    public AudioSource backgrumdMusic;
+    public static SoundManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if(instance != this)
+            Destroy(gameObject);
+    }
+
+    void PlayMusic(AudioClip clip)
+    {
+        effectMusic.clip = clip;
+        effectMusic.Play();
+    }
+}
